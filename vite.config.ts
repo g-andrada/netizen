f58ac6b5daf7bc46524fb4e7bc4b73686eab6491
Gideon Andrada <andrada.gideon@gmail.com>
2026-04-10 14:51:06 -0400

    Re-configured Vite Server
    - Pull VITE_HOST from .env file to dynamically configure CORS and HMR

--- vite.config.ts
+++ vite.config.ts
@@ -2,44 +2,47 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+
+
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            tailwindcss(),
+            wayfinder({
+                formVariants: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        server: {
+            host: '0.0.0.0', // Accept connection from any device on the network
+            hmr: {
+                host: `${env.VITE_HOST}` // Hor reload for Websockets
             },
-        }),
-    ],
-    server: {
-        host: '0.0.0.0', // Listen to all network interfaces
-        hmr: {
-            // host: '10.0.0.171'
-            host: '10.0.0.170'
-        },
-        cors: {
-            origin: [
-                'http://localhost:8000',
-                'http://10.0.0.171:8000',
-                'http://10.0.0.171:5173',
-                'http://10.0.0.170:8000',
-                'http://10.0.0.170:5173'
-            ],
-            methods: [ 'GET', 'POST', 'PUT', 'DELETE' ],
-            allowedHeaders: [ 'Content-Type', 'Authorization' ]
+            cors: {
+                origin: [
+                    `http://${env.VITE_HOST}:8000`, // Web server
+                    `http://${env.VITE_HOST}:5173`, // Vite server
+                ],
+                // origin: true,
+                methods: [ 'GET', 'POST', 'PUT', 'DELETE' ],
+                allowedHeaders: [ 'Content-Type', 'Authorization' ]
+            }
         }
     }
 });
